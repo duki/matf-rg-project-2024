ec5a38ea4c69246598046003a1cb130224f30d75
duki <mi19325@alas.matf.bg.ac.rs>
2026-08-18 22:55:59 +0200

    dodao prvi kontroler

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -1,8 +1,34 @@
 #include <engine/core/Engine.hpp>
+#include <engine/graphics/GraphicsController.hpp>
+#include <spdlog/spdlog.h>
 
-/**
- * Start here...
- */
+class RoomController final : public engine::core::Controller {
+public:
+    std::string_view name() const override {
+        return "RoomController";
+    }
+
+protected:
+    void initialize() override {
+        engine::graphics::OpenGL::enable_depth_testing();
+        spdlog::info("RoomController initialized.");
+    }
+
+    void draw() override {
+        auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+        engine::graphics::OpenGL::clear_buffers();
+        platform->swap_buffers();
+    }
+};
+
+class MainApp final : public engine::core::App {
+protected:
+    void app_setup() override {
+        auto room_controller = register_controller<RoomController>();
+        room_controller->after(engine::core::Controller::get<engine::core::EngineControllersEnd>());
+    }
+};
+
 int main(int argc, char **argv) {
-    return 0;
+    return std::make_unique<MainApp>()->run(argc, argv);
 }
