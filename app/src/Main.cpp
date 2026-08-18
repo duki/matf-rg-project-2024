d58432e59981c04d81367176496fd373d206f1b7
duki <mi19325@alas.matf.bg.ac.rs>
2026-08-18 23:34:07 +0200

    iscrtavamo model poda, dodat starter mvp shader

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -1,5 +1,6 @@
 #include <engine/core/Engine.hpp>
 #include <engine/graphics/GraphicsController.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <spdlog/spdlog.h>
 
 
@@ -55,6 +56,7 @@ protected:
         spdlog::info("RoomController initialized.");
     }
 
+
     void poll_events() override {
         auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
 
@@ -90,12 +92,45 @@ protected:
     }
 
     void draw() override {
+        auto rc = engine::core::Controller::get<engine::resources::ResourcesController>();
+        auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
         auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+        auto camera = graphics->camera();
+
+
         engine::graphics::OpenGL::clear_buffers();
+
+
+        auto shader = rc->shader("starter");
+        if (shader != nullptr) {
+            shader->use();
+
+
+            glm::mat4 projection = graphics->projection_matrix();
+            glm::mat4 view = camera->view_matrix();
+            shader->set_mat4("projection", projection);
+            shader->set_mat4("view", view);
+
+            auto floor_model = rc->model("floor");
+            glm::mat4 model_mat = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+            draw_model(shader, floor_model, model_mat, glm::vec3(0.78f, 0.62f, 0.45f));
+        }
+
+
         platform->swap_buffers();
     }
 
 private:
+    void draw_model(const engine::resources::Shader *shader, engine::resources::Model *model, const glm::mat4 &transform, const glm::vec3 &color) {
+        if (model == nullptr) {
+            return;
+        }
+
+        shader->set_mat4("model", transform);
+        shader->set_vec3("objectColor", color);
+        model->draw(shader);
+    }
+
     bool m_mouse_captured{true};
 };
 
