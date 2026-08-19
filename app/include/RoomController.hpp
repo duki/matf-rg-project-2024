b4214a43f3e82fd9bb181dac72cf33d10e5e8aba
duki <mi19325@alas.matf.bg.ac.rs>
2026-08-19 21:15:41 +0200

    modularizovao kod

---
+++ app/include/RoomController.hpp
@@ -0,0 +1,58 @@
+#ifndef ROOM_CONTROLLER_HPP
+#define ROOM_CONTROLLER_HPP
+
+#include <LightSettings.hpp>
+#include <engine/core/Controller.hpp>
+#include <engine/graphics/Camera.hpp>
+#include <engine/platform/PlatformEventObserver.hpp>
+#include <engine/resources/Model.hpp>
+#include <engine/resources/Shader.hpp>
+#include <memory>
+
+class MainPlatformEventObserver final : public engine::platform::PlatformEventObserver {
+public:
+    explicit MainPlatformEventObserver(engine::graphics::Camera *camera, bool *mouse_captured);
+
+    void on_mouse_move(engine::platform::MousePosition position) override;
+    void on_scroll(engine::platform::MousePosition position) override;
+
+private:
+    engine::graphics::Camera *m_camera{nullptr};
+    bool *m_mouse_captured{nullptr};
+};
+
+class RoomController final : public engine::core::Controller {
+public:
+    std::string_view name() const override {
+        return "RoomController";
+    }
+
+protected:
+    void initialize() override;
+    void poll_events() override;
+    void update() override;
+    void draw() override;
+
+private:
+    void draw_gui();
+    void draw_model(const engine::resources::Shader *shader, engine::resources::Model *model, const glm::mat4 &transform, const glm::vec3 &color);
+    void trigger_movie_mode();
+    void reset_mode();
+
+    DirLightSettings m_dir_light{};
+    PointLightSettings m_point_light{};
+
+    // stanje tajmera za filmski mod
+    bool m_movie_mode_triggered{false};
+    float m_event_timer{0.0f};
+    bool m_event_a_done{false};
+    bool m_event_b_done{false};
+    float m_tv_glow{0.0f};
+    // kraj stanja tajmera za filmski mod
+
+    bool m_mouse_captured{true};
+    bool m_draw_gui{true};
+    bool m_spotlight_attached_to_camera{true};
+};
+
+#endif//ROOM_CONTROLLER_HPP
